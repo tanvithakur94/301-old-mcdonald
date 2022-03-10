4cf3201b4f91057a8daf76196c5977d9672a8e99
Tanvi <tanvithakur94@vt.edu>
2022-03-10 02:40:41 +0000

    Changed app.py

--- app.py
+++ app.py
@@ -13,14 +13,16 @@ sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
 # here's the list of possible columns to choose from.
 list_of_columns =['total exports', 'beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+      # 'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
+       #'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+
 
 
 ########## Set up the chart
 
 import pandas as pd
 df = pd.read_csv('assets/usa-2011-agriculture.csv')
+#df = pd.read_csv('assets/shootings.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -56,7 +58,8 @@ app.layout = html.Div(children=[
              [Input('options-drop', 'value')])
 def make_figure(varname):
     mygraphtitle = f'Exports of {varname} in 2011'
-    mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+    #mycolorscale = 'ylorrd'# Note: The error message will list possible color scales.
+    mycolorscale = 'PiYG'
     mycolorbartitle = "Millions USD"
 
     data=go.Choropleth(
@@ -81,70 +84,3 @@ if __name__ == '__main__':
     app.run_server(debug=True)
 
 
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
-# import plotly.graph_objs as go
-# import pandas as pd
-
-# ########### Define your variables ######
-
-# # here's the list of possible columns to choose from.
-# list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 'poultry',
-#        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-#        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
-
-# mycolumn='corn'
-# myheading1 = "2011 Agricultura Exports, by State"
-# #myheading1 = f"Wow! That's a lot of {mycolumn}!"
-# mygraphtitle = '2011 US Agriculture Exports by State'
-# mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-# mycolorbartitle = "Millions USD"
-# tabtitle = 'Old McDonald'
-# sourceurl = 'https://plot.ly/python/choropleth-maps/'
-# githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
-
-
-# ########## Set up the chart
-
-# import pandas as pd
-# df = pd.read_csv('assets/usa-2011-agriculture.csv')
-
-# fig = go.Figure(data=go.Choropleth(
-#     locations=df['code'], # Spatial coordinates
-#     z = df[mycolumn].astype(float), # Data to be color-coded
-#     locationmode = 'USA-states', # set of locations match entries in `locations`
-#     colorscale = mycolorscale,
-#     colorbar_title = mycolorbartitle,
-# ))
-
-# fig.update_layout(
-#     title_text = mygraphtitle,
-#     geo_scope='usa',
-#     width=1200,
-#     height=800
-# )
-
-# ########### Initiate the app
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
-# app.title=tabtitle
-
-# ########### Set up the layout
-
-# app.layout = html.Div(children=[
-#     html.H1(myheading1),
-#     dcc.Graph(
-#         id='figure-1',
-#         figure=fig
-#     ),
-#     html.A('Code on Github', href=githublink),
-#     html.Br(),
-#     html.A("Data Source", href=sourceurl),
-#     ]
-# )
-
-# ############ Deploy
-# if __name__ == '__main__':
-#     app.run_server()
