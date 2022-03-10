3a813022e913b36b77a4d50b89fd1d9e19c424de
Tanvi <tanvithakur94@vt.edu>
2022-03-10 04:26:47 +0000

    Changed app.py

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ tabtitle = 'Old McDonald'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
 # here's the list of possible columns to choose from.
-list_of_columns =['beef', 'pork', 'poultry',
-       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
-       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_columns =['fruits', 'veggies fresh',
+       'veggies proc', 'corn', 'wheat', 'cotton', 'strawberry']
+
+#list_of_columns =['beef', 'pork', 'poultry',
+       #'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
+       #'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
 ########## Set up the chart
 
@@ -33,7 +36,7 @@ app.layout = html.Div(children=[
     html.H1('2012 Agricultural Exports, by State',style={'textAlign': 'center'}),
     html.Div([
         html.Div([
-                html.H6('Select a variable for analysis:'),
+                html.H6('Select a variable for analysis:', style={'textAlign': 'center'}),
                 dcc.Dropdown(
                     id='options-drop',
                     options=[{'label': i, 'value': i} for i in list_of_columns],
