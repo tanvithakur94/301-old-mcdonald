cbc74e7e9f1b33740bfba629b1af6dcec127922d
Tanvi <tanvithakur94@vt.edu>
2022-03-10 02:43:11 +0000

    Changed app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-
 import plotly.graph_objs as go
 import pandas as pd
 
